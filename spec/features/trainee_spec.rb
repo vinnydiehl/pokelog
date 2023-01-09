35ac7576ae3dc2fa2622a4aafbbe6e4df7be14d4
vinnydiehl <vinny.diehl@gmail.com>
2023-01-09 17:29:46 -0500

    Refactor sprite image_tag logic to Species model

---
+++ spec/features/trainee_spec.rb
@@ -0,0 +1,14 @@
+require "rails_helper"
+
+RSpec.feature "/trainee/#:", type: :feature do
+  it "displays the trainee" do
+  end
+
+  context "while logged out" do
+    it "doesn't allow you to edit the trainee" do
+    end
+  end
+
+  context "while logged in" do
+  end
+end
