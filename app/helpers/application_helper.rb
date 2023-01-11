287c4502d4cab41bd831f173b31032e249877306
vinnydiehl <vinny.diehl@gmail.com>
2023-01-11 07:38:39 -0500

    Improve client-side input checks
    Character counter, maxlength (even hacked onto numeric inputs),
    validation for EV inputs.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,10 @@
 module ApplicationHelper
+  # Generates a nav link for the sidenav, with an icon and optional text.
+  #
+  # @param icon [String] material-icons ligature
+  # @param path [String] target path
+  # @param text [String] text (for wide sidebar)
+  # @return [String] HTML for the sidenav li element
   def nav_link(icon, path, text = "")
     attrs = { class: "bold" }
 
@@ -9,5 +15,16 @@ module ApplicationHelper
         content_tag(:i, icon, class: "material-icons").concat(content_tag :div, text)
       end
     end
+  end
+
+  # Numeric inputs ignore the maxlength property. This bit of JS gets around that.
+  # Splat it onto your options, like:
+  #
+  #   = form.text_field :level, type: "number", min: 1, max: 100, **maxlength(3)
+  #
+  # @param n [Integer] number of characters to limit input to
+  # @return [Hash] option to pass to #text_field to limit characters
+  def maxlength(n)
+    { onKeyPress: "if (this.value.length > #{n - 1}) return false;" }
   end
 end
