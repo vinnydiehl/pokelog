5f352dbd3021af992056261f7f6984857405d3cd
vinnydiehl <vinny.diehl@gmail.com>
2023-01-17 09:20:25 -0500

    Make items/Pokérus work
    Using JavaScript still, but made the kill_button way slimmer by
    refactoring to a function.

---
+++ app/javascript/update_evs.js
@@ -0,0 +1,35 @@
+function updateEvs(hp, atk, def, spa, spd, spe) {
+    switch (document.querySelector(".items-options input[type='radio']:checked").id) {
+    case "trainee_item_macho_brace":
+        hp *= 2; atk *= 2; def *= 2; spa *= 2; spd *= 2; spe *= 2;
+        break;
+    case "trainee_item_power_weight":
+        hp += 4;
+        break;
+    case "trainee_item_power_bracer":
+        atk += 4;
+        break;
+    case "trainee_item_power_belt":
+        def += 4;
+        break;
+    case "trainee_item_power_lens":
+        spa += 4;
+        break;
+    case "trainee_item_power_band":
+        spd += 4;
+        break;
+    case "trainee_item_power_anklet":
+        spe += 4;
+        break;
+    default: break;
+    }
+    if (document.getElementById("trainee_pokerus").checked) {
+        hp *= 2; atk *= 2; def *= 2; spa *= 2; spd *= 2; spe *= 2;
+    }
+
+    var inputs = document.querySelectorAll(".point input");
+    inputs.forEach(input => {
+    input.value = parseInt(input.value) + eval(input.parentElement.classList[1]);
+    });
+    inputs[0].dispatchEvent(new Event('input',{bubbles:true}));
+}
