de6a05dd65e82b00b588abc0b58dbd5f071fdc53
vinnydiehl <vinny.diehl@gmail.com>
2023-02-12 01:52:49 -0500

    Registration validation improvements
    Better error display, removed redundant code. Also set permanent cookie
    for session.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,13 @@
 class User < ApplicationRecord
   has_many :trainees
 
-  validates_presence_of :username
-  validates_uniqueness_of :username
-  validates :username, length: 1..20
+  validates_presence_of :username, message: "can't be blank"
+  validates_uniqueness_of :username, message: "has already been taken"
+  validates :username,
+    length: { maximum: 20, message: "must be 20 characters or less" }
 
-  validates_presence_of :email
-  validates :email, email: true
+  validates :email, email: { mode: :strict, message: "is invalid" },
+    length: { maximum: 255, message: "must be 255 characters or less" }
 
   def to_param
     username
