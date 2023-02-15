f35e7e7474022988ff50b0b031785c85dc3548e8
vinnydiehl <vinny.diehl@gmail.com>
2023-02-14 20:06:04 -0500

    Implement generation selector client-side
    This just sets a cookie with JS, it's not really part of the form.
    
    refs #26

--- app/javascript/controllers/form_submission_controller.js
+++ app/javascript/controllers/form_submission_controller.js
@@ -9,4 +9,23 @@ export default class extends Controller {
     submit() {
         event.target.closest("form").requestSubmit();
     }
+
+    // This select (in the filters modal) just sets a cookie and refreshes the form
+    generationSubmit() {
+        const gen = document.getElementById("generation").value;
+
+        // If a gen is selected, permanent cookie. Otherwise delete any existing cookie
+        const expire_date = gen ? new Date(Date.now() + (20 * 365 * 24 * 60 * 60 * 1000))
+                                : new Date(0);
+
+        document.cookie = `generation=${gen}; path=/; SameSite=Lax; expires=${expire_date}`;
+
+        // Stop "generation" from showing in params
+        document.getElementById("generation").disabled = true;
+        this.submit(event);
+
+        // It looks like this gets re-enabled on submit, but if you go back
+        // then forward, it would break
+        document.getElementById("generation").disabled = false;
+    }
 }
