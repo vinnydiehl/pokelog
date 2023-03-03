1cd857fd2cf3274d809244e6360ceba609a08a7f
vinnydiehl <vinny.diehl@gmail.com>
2023-03-02 22:04:09 -0500

    Clean cookies from deleted trainees
    This is checked on every page load, since I don't want stale cookies
    sitting around if the user deletes the trainee on a different device.
    
    refs #35

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  before_action *%i[init_globals set_current_user turbo_request_variant]
+  before_action *%i[init_globals set_current_user turbo_request_variant clean_cookies]
 
   private
 
@@ -18,5 +18,17 @@ class ApplicationController < ActionController::Base
 
   def turbo_request_variant
     request.variant = :turbo_frame if turbo_frame_request?
+  end
+
+  def clean_cookies
+    if helpers.logged_in?
+      # Detect and remove old trainee EV goal cookies
+      cookies.each do |name, value|
+        if name.starts_with?("trainee_") &&
+           @current_user.trainees.where(id: name.split("_")[1].to_i).none?
+          cookies.delete name
+        end
+      end
+    end
   end
 end
