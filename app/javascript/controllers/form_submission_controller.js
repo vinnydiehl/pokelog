c6e121b257135c6b90a872256a94e95482acd603
Vinny Diehl <vinny.diehl@gmail.com>
2023-06-13 14:34:23 -0400

    Connect generation widget

--- app/javascript/controllers/form_submission_controller.js
+++ app/javascript/controllers/form_submission_controller.js
@@ -1,5 +1,6 @@
 import { Controller } from "@hotwired/stimulus"
 
+// Connects to data-controller="form-submission"
 export default class extends Controller {
     searchSubmit() {
         clearTimeout(this.timeout);
@@ -8,21 +9,5 @@ export default class extends Controller {
 
     submit() {
         event.target.closest("form").requestSubmit();
-    }
-
-    // This select (in the filters modal) just sets a cookie and refreshes the form
-    generationSubmit() {
-        const gen = parseInt(document.getElementById("generation").value);
-
-        // If a gen is selected, permanent cookie. Otherwise delete any existing cookie
-        const expire_date = gen ? new Date(Date.now() + (20 * 365 * 24 * 60 * 60 * 1000))
-                                : new Date(0);
-
-        document.cookie = `generation=${gen}; path=/; SameSite=Lax; expires=${expire_date}`;
-
-        M.Modal.getInstance(document.getElementById("species-filters")).close();
-
-        // Reload entire page, this way we update the tooltips, etc.
-        Turbo.visit(window.location.href, {action: "replace"});
     }
 }
