3014098eddf2facb0ee421111a500962ca20da2a
Vinny Diehl <vinny.diehl@gmail.com>
2023-06-15 03:47:00 -0400

    Remove old function from form submission controller

--- app/javascript/controllers/form_submission_controller.js
+++ app/javascript/controllers/form_submission_controller.js
@@ -1,29 +1,20 @@
+/**
+  * Functions for submitting forms.
+  **/
+
 import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="form-submission"
 export default class extends Controller {
+  // Submits after a 200ms delay, good for input events, as you don't want
+  // to submit on every keypress
   searchSubmit() {
     clearTimeout(this.timeout);
     this.timeout = setTimeout(() => { this.element.requestSubmit() }, 200);
   }
 
+  // Submits immediately
   submit() {
     event.target.closest("form").requestSubmit();
-  }
-
-  // This select (in the filters modal) just sets a cookie and refreshes the form
-  generationSubmit() {
-    const gen = parseInt(document.getElementById("generation").value);
-
-    // If a gen is selected, permanent cookie. Otherwise delete any existing cookie
-    const expire_date = gen ? new Date(Date.now() + (20 * 365 * 24 * 60 * 60 * 1000))
-                            : new Date(0);
-
-    document.cookie = `generation=${gen}; path=/; SameSite=Lax; expires=${expire_date}`;
-
-    M.Modal.getInstance(document.getElementById("species-filters")).close();
-
-    // Reload entire page, this way we update the tooltips, etc.
-    Turbo.visit(window.location.href, {action: "replace"});
   }
 }
