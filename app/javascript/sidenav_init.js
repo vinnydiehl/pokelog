9fbb68ee86e3707fd7d8d4b3e5bece7cac69bf03
Vinny Diehl <vinny.diehl@gmail.com>
2023-06-15 05:02:14 -0400

    Use Hotwire for sidenav
    This one is really nice.

--- app/javascript/sidenav_init.js
+++
@@ -1,7 +0,0 @@
-runOnLoad(() => {
-    M.Sidenav.init(document.querySelectorAll(".sidenav"));
-    if (window.innerWidth <= 992) {
-        document.querySelector('#sidenav').style.transform = "translateX(-105%)";
-        document.querySelector('#sidenav').style.display = "block";
-    }
-});
