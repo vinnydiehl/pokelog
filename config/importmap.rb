0523ebb5dfd5f6831c9e7f2d632d73d38d4aef94
Vinny Diehl <vinny.diehl@gmail.com>
2023-06-17 02:41:41 -0400

    Use Hotwire for radar charts
    fixes #6

--- config/importmap.rb
+++ config/importmap.rb
@@ -8,5 +8,6 @@ pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
 
+pin "anychart-radar", to: "anychart_radar.min.js", preload: true
 pin "nouislider", to: "nouislider.min.js", preload: true
 pin "slip", to: "slip.js", preload: true
